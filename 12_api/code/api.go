5ac308860a75ba08ab7faad8da27e21db1520c8c
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-07-21 18:23:43 -0500

    Completed server, API, and concurrency modules.

--- 12_api/code/api.go
+++ 12_api/code/api.go
@@ -1,12 +1,77 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 // BaseURL is the base endpoint for the star wars API
 const BaseURL = "https://swapi.dev/api/"
 
+// Planet is a world
+type Planet struct {
+	Name       string `json:"name"`
+	Population string `json:"population"`
+	Terrain    string `json:"terrain"`
+}
+
+// Person is a person
+type Person struct {
+	Name         string `json:"name"`
+	HomeworldURL string `json:"homeworld"`
+	Homeworld    Planet
+}
+
+// AllPeople has people
+type AllPeople struct {
+	People []Person `json:"results"`
+}
+
+func (p *Person) getHomeworld() {
+	res, err := http.Get(p.HomeworldURL)
+	if err != nil {
+		log.Print("Error fetchign homeworld", err)
+	}
+
+	var bytes []byte
+	if bytes, err = ioutil.ReadAll(res.Body); err != nil {
+		log.Print("Error reading response body", err)
+	}
+
+	json.Unmarshal(bytes, &p.Homeworld)
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	res, err := http.Get(BaseURL + "people")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Failed to request people")
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Failed to parse request body")
+	}
+
+	var people AllPeople
+	if err := json.Unmarshal(bytes, &people); err != nil {
+		fmt.Println("Error parsing json", err)
+	}
+	fmt.Println(people)
+
+	for _, pers := range people.People {
+		pers.getHomeworld()
+		fmt.Println(pers)
+	}
+}
+
 func main() {
-	fmt.Println(BaseURL)
+	http.HandleFunc("/people", getPeople)
+	fmt.Println("Serving on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
